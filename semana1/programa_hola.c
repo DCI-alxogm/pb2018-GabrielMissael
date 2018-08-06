d2234dabe3fd95b0140cfbd5e9fa9b7d091fa048
Gabriel Missael Barco <missael_gabo@hotmail.com>
2018-08-06 10:00:42 -0500

    Añadi un arhivo#

---
+++ semana1/programa_hola.c
@@ -0,0 +1,6 @@
+/*
+	Esto es un comentario. Me sirve para escribir información relevante del programa que estoy
+	haciendo. Por ejemplo:
+	Creado el 06/08/18 por Gabriel Missael Barco, estudiante de la licenciatura en Física en la 
+	Universidad de Guanajuato
+*/
