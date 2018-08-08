dda02c873366ed29c23b82ffbbbbf8f56c1c7478
Alma Gonzalez <alxogm@Almas-MacBook-Air-2.local>
2018-08-08 09:25:42 -0500

    Cree mi primer programa.

--- semana1/programa_hola.c
+++ semana1/programa_hola.c
@@ -1,3 +1,15 @@
 /*Este es un comentario
-Progrma creado el 06/08/2018 by Alma Gonzalez
+Programa creado el 06/08/2018 by Alma Gonzalez
 */
+
+#include<stdio.h>
+
+int main()
+{
+    
+    printf("Hola Alma \n");
+    printf("Este es el primer programa del curso\n");
+    
+    }
+    
+/*Termine de escribir el programa*/
