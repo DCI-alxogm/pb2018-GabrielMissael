133565ba2aa739b1ff78d893dacb910fb4d65162
Gabriel Missael Barco <missael_gabo@hotmail.com>
2018-08-08 09:25:43 -0500

    Cree mi primer programa, en la carpeta semana 1

---
+++ semana1/primer_programa.c
@@ -0,0 +1,11 @@
+/*Creado el 08 de Agosto del 2018*/
+
+#include <stdio.h>
+
+int main(){
+
+	printf("Hola Missael \n");
+	printf("Este es el primer programa del curso \n");
+}
+
+/*Termine de escribir el programa*/
