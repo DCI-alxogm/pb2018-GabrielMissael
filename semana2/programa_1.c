1143a15ce65143618720efd5b04648622b8f0cfa
Gabriel Missael Barco <missael_gabo@hotmail.com>
2018-08-15 09:58:44 -0500

    Actualicé el programa1.out

--- semana2/programa_1.c
+++ semana2/programa_1.c
@@ -4,13 +4,16 @@
 
 int main(){
 	int edad=0, aux=0;
+	char nombre[30];
 	
 	printf("Holaaaaa, estimado usuario :)  \n");
 	printf("Este es el primer programa interactivo del curso. Introduce tu edad: ");
 	scanf("%i", &edad);
-	printf("\n Tu edad es: %i \n", edad);
+	printf("\n Introduce tu nombre: ");
+	scanf("%s",&nombre);
+	printf("\n %s, tú edad es: %i \n", nombre, edad);
 	aux = edad+10;
-	printf("\n Tu edad en diez año,es decir, en el 2028,  será: %i \n", aux);
+	printf("\n Tú edad en diez años, es decir, en el 2028,  será: %i \n", aux);
 
 	return 0;
 }
