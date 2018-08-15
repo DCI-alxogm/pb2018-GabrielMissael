2da106df6e1868b066d01850ea8eefa6537b8040
Gabriel Missael Barco <missael_gabo@hotmail.com>
2018-08-15 08:02:21 -0500

    Subí rograma de la clase pasada (13/08/18)

---
+++ semana2/programa_1.c
@@ -0,0 +1,16 @@
+//Creado el 13/08/18 por Gabriel Missael Barco
+
+#include <stdio.h>
+
+int main(){
+	int edad=0, aux=0;
+	
+	printf("Hola Estimado usuario \n");
+	printf("Este es el primer programa interactivo del curso. Introduce tu edad: ");
+	scanf("%i", &edad);
+	printf("\n Tu edad es: %i \n", edad);
+	aux = edad+10;
+	printf("\n Tu edad en diez años será: %i", aux);
+
+	return 0;
+}
