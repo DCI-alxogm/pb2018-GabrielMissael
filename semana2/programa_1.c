985f5565db5d30837dc4443714306824bc9d5df2
Gabriel Missael Barco <missael_gabo@hotmail.com>
2018-08-15 08:37:23 -0500

    Actualicé el programa_1.c

--- semana2/programa_1.c
+++ semana2/programa_1.c
@@ -5,7 +5,7 @@
 int main(){
 	int edad=0, aux=0;
 	
-	printf("Hola Estimado usuario \n");
+	printf("Holaaaaa, estimado usuario :)  \n");
 	printf("Este es el primer programa interactivo del curso. Introduce tu edad: ");
 	scanf("%i", &edad);
 	printf("\n Tu edad es: %i \n", edad);
